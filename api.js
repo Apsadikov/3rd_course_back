a0ad5a736aced784756213938949443f7eedbcf5
ShGulnaz <shakirova139@gmail.com>
2021-04-15 21:04:05 +0300

    fix getCookie

--- api.js
+++ api.js
@@ -29,7 +29,11 @@ function setCookie(json) {
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift()
+    } else {
+        return 'undefined';
+    }
 }
 
 function checkCookie() {
